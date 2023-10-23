f10e6ef18aee29ff33b02027e68b4401c889e661
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-10-23 14:52:06 +0100

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Python script that use a given REST API"""
+import json
 import requests
 import sys
-import json
 
 if __name__ == "__main__":
     user_id = eval(sys.argv[1])
