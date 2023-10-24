39d26e81d09338358340bd419971744f34431f8a
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-10-25 00:14:54 +0100

    task3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,19 +7,18 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/todos"
     response = requests.get(url).text
     res_list = json.loads(response)
-    
+
     user_name = []
     for i in range(1, 11):
         user_url = "https://jsonplaceholder.typicode.com/users/{}".format(i)
         user_response = requests.get(user_url).text
         user_dict = json.loads(user_response)
         user_name.append(user_dict["username"])
-   # print(user_name)
 
     count = 0
     new_res_list = [
-            [],[],[],[],[],
-            [],[],[],[],[]
+            [], [], [], [], [],
+            [], [], [], [], []
             ]
 
     for elem in res_list:
@@ -34,7 +33,6 @@ if __name__ == "__main__":
                 break
 
     final_dict = {str(k): v for k, v in zip(range(1, 11), new_res_list)}
-    # print(final_dict)
-    
+
     with open("todo_all_employees.json", "w") as file:
         json.dump(final_dict, file)
