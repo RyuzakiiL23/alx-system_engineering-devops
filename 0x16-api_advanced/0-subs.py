f2b6b6228e159b6ae2945192e8ad05e0c4503ab4
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-11-08 02:12:24 +0100

    task 1 2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,8 +5,11 @@ import requests
 def number_of_subscribers(subreddit):
     """Query that returns the number of redit subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    # headers = {
+    #     "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    # }
     response = requests.get(url, headers=None, allow_redirects=False)
     if response.status_code == 404:
         return 0
-    data = response.json().get("data")
-    return data.get("subscribers")
+    results = response.json().get("data")
+    return results.get("subscribers")
